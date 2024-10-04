65b2cbf522c320176ec737fb8fa5a1f1286529b7
Robert Fry <inbox@robertfry.xyz>
2024-10-04 02:15:26 +0100

    refact: move particle physics to the particle system

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,7 +1,6 @@
 
 use bevy::prelude::*;
 use bevy::sprite::*;
-use bevy::window::*;
 
 use crate::settings::*;
 use crate::state::*;
@@ -28,16 +27,6 @@ impl Plugin for Simulation
             Simulation::respawn_particle_grid
             .run_if(on_event::<SettingsChangedEvent>())
             .run_if(in_state(SimState::Configure))
-            );
-
-        app.add_systems(Update,
-            (
-                Simulation::on_gravity,
-                Simulation::movement,
-                Simulation::confine_to_window,
-            )
-            .chain()
-            .run_if(in_state(SimState::Running))
             );
     }
 }
@@ -78,67 +67,6 @@ impl Simulation
                     velocity: Vec2::new(0.0, 0.0),
                 },
             ));
-        }
-    }
-
-    fn movement(
-        mut particles: Query<(&mut Transform, &Particle)>,
-        time: Res<Time>,
-    ){
-        for (mut transform, particle) in particles.iter_mut()
-        {
-            let delta = particle.velocity * time.delta_seconds();
-            transform.translation += Vec3::new(delta.x, delta.y, 0.0);
-        }
-    }
-
-    fn confine_to_window(
-        mut particles: Query<(&mut Transform, &mut Particle)>,
-        window_query: Query<&Window, With<PrimaryWindow>>,
-        settings: Res<Settings>,
-    ){
-        let window = window_query.get_single().unwrap();
-
-        let bounds_min_x = -window.width()  / 2.0 + settings.particle_radius;
-        let bounds_max_x =  window.width()  / 2.0 - settings.particle_radius;
-        let bounds_min_y = -window.height() / 2.0 + settings.particle_radius;
-        let bounds_max_y =  window.height() / 2.0 - settings.particle_radius;
-
-        for (mut transformation, mut particle) in particles.iter_mut()
-        {
-            // make the particle bounce
-            if
-                transformation.translation.x <= bounds_min_x ||
-                transformation.translation.x >= bounds_max_x
-            {
-                particle.velocity.x *= -1.0 * (1.0 - settings.border_damping);
-            }
-            if
-                transformation.translation.y <= bounds_min_y ||
-                transformation.translation.y >= bounds_max_y
-            {
-                particle.velocity.y *= -1.0 * (1.0 - settings.border_damping);
-            }
-
-            // clamp the particles position inside the window
-            transformation.translation = transformation.translation.clamp(
-                Vec3::new(bounds_min_x, bounds_min_y, 0.0),
-                Vec3::new(bounds_max_x, bounds_max_y, 0.0),
-            );
-        }
-    }
-
-    fn on_gravity(
-        mut particles: Query<&mut Particle>,
-        settings: Res<Settings>,
-        time: Res<Time>
-    ){
-        let gravity = -1.0 * settings.gravity * settings.force_multiplier;
-        let gravity_vector = Vec2::new(0.0, gravity);
-
-        for mut particle in particles.iter_mut()
-        {
-            particle.velocity += gravity_vector * time.delta_seconds();
         }
     }
 }
