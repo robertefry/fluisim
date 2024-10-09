2c90f7613139ff0f7ee263eee3e09f6d586b6ee3
Robert Fry <inbox@robertfry.xyz>
2024-10-10 00:06:21 +0100

    fix: euclidean distance calculation

--- util/src/euclidean.rs
+++ util/src/euclidean.rs
@@ -2,7 +2,7 @@
 pub fn distance<const N: usize>(pos1: &[f32;N], pos2: &[f32;N]) -> f64
 {
     itertools::izip!(pos1, pos2)
-        .map(|(a,b)| (a-b).powi(2) as f64)
+        .map(|(a,b)| ((a-b) as f64).powi(2))
         .sum::<f64>()
         .sqrt()
 }
